4da3c2c7e7c1cffd8d63f230ba82faedcdeadce4
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-05-12 18:09:56 -0700

    style: use native `JSON.parse`

--- src/decode.js
+++ src/decode.js
@@ -1,10 +1,9 @@
 // @flow
 import assert from 'assert';
 import {Base64} from 'js-base64';
-import parseJson from 'parse-json';
 
 export default function decode(text: string): any {
   assert(typeof text === 'string', '"text" must be a string.');
   const json = Base64.decode(text);
-  return parseJson(json);
+  return JSON.parse(json);
 }
