18450cae3b57e554eefe8e413678e9b6097491ba
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-05-12 18:16:36 -0700

    perf: use `fast-json-stable-stringify`

--- src/encode.js
+++ src/encode.js
@@ -1,6 +1,6 @@
 // @flow
 import {Base64} from 'js-base64';
-import stringify from 'json-stable-stringify';
+import stringify from 'fast-json-stable-stringify';
 
 export default function encode(value: any): string {
   const json = stringify(value);
